add90a26644e2c912998ebf0c607d269d9b63dd0
Stevan-LS <stevan.lestanc@gmail.com>
2024-06-04 15:45:26 +0800

    little_modif

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
 const express = require('express')
+const lark = require("@larksuiteoapi/node-sdk");
+const axios = require("axios");
+
+const LARK_APP_ID = process.env.LARK_APP_ID; // Larksuite appid 
+const LARK_APP_SECRET = process.env.LARK_APP_SECRET; // larksuite app secret
+const COZE_PAT = process.env.COZE_PAT;
+const BOT_ID = process.env.BOT_ID;
+const MAX_TOKEN = process.env.MAX_TOKEN;
+
 const app = express()
 const port = process.env.PORT || 4000;
 
-const lark = require("@larksuiteoapi/node-sdk");
-
-const axios = require("axios");
-
-const LARK_APP_ID = 'cli_a6df832907f8d010'; // Larksuite appid 
-
-const LARK_APP_SECRET = 'q8wXFw1TaIvUDCQnIvprOf1aLlEZvBlj'; // larksuite app secret
-
-const COZE_PAT = 'pat_fiGlPSAhjuypBdrskqa0mrk1xuG4AHVfH4HTQsU3ycrd05AKUO5DVRdctCzXOV65'; // Coze personal access token
-
-const BOT_ID = '7375049088703741960';
-const MAX_TOKEN = 1024; //  Max token param 
-
 // Middleware to parse JSON bodies
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.post('/', async (req, res) => {
   try {
@@ -35,7 +27,7 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
 
 
@@ -92,9 +84,6 @@ async function reply(messageId, content) {
   }
 
 }
-
-
-
 
 
 // Use Session ID to build coversation
